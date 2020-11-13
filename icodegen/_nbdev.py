c85a71bc3dbfd71f143ef05647f50e77f9b257ea
Nathan Cooper <nac33@students.uwf.edu>
2020-11-12 20:55:19 -0500

    Update docs and lib

--- icodegen/_nbdev.py
+++ icodegen/_nbdev.py
@@ -2,32 +2,27 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"remove_non_ascii": "00_data.ipynb",
-         "beautify_code": "00_data.ipynb",
-         "extra_tokens": "00_data.ipynb",
-         "java_reserved_tokens": "00_data.ipynb",
-         "java_operator_tokens": "00_data.ipynb",
-         "java_structural_tokens": "00_data.ipynb",
-         "java_extra_tokens": "00_data.ipynb",
-         "java_special_tokens": "00_data.ipynb",
-         "replace_special_tokens": "00_data.ipynb",
-         "train_tokenizer": "00_data.ipynb",
+index = {"remove_non_ascii": "00_data.core.ipynb",
+         "beautify_code": "00_data.core.ipynb",
+         "extra_tokens": "00_data.core.ipynb",
+         "java_reserved_tokens": "00_data.core.ipynb",
+         "java_operator_tokens": "00_data.core.ipynb",
+         "java_structural_tokens": "00_data.core.ipynb",
+         "java_extra_tokens": "00_data.core.ipynb",
+         "java_special_tokens": "00_data.core.ipynb",
+         "replace_special_tokens": "00_data.core.ipynb",
+         "train_tokenizer": "00_data.core.ipynb",
          "java_comment_remover": "00_data.transforms.ipynb",
          "line_randomizer": "00_data.transforms.ipynb",
          "code_token_randomizer": "00_data.transforms.ipynb",
          "transform_df": "00_data.transforms.ipynb",
          "Model": "01_model.ipynb",
          "TransformerModel": "01_model.ipynb",
-         "GRUModel": "01_model.ipynb",
-         "get_mean_probs": "02_experiments.ipynb",
-         "find_parens": "02_experiments.ipynb",
-         "mean_dist_probs": "02_experiments.ipynb",
-         "get_mean_cross_entropy": "02_experiments.ipynb"}
+         "GRUModel": "01_model.ipynb"}
 
-modules = ["data.py",
+modules = ["data/core.py",
            "data/transforms.py",
-           "model.py",
-           "experiments.py"]
+           "model.py"]
 
 doc_url = "https://danaderp.github.io/icodegen/"
 

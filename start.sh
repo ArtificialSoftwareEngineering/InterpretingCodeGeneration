a6fc6dc63989567afdfa78715dde989cc575fd65
ncoop57 <nacooper01@email.wm.edu>
2021-02-09 20:51:37 -0500

    Update start script for now

--- start.sh
+++ start.sh
@@ -3,18 +3,20 @@
 PORT=$1
 TAG=icodegen
 
-if [ $# -eq 3 ]; then
-	if [ "$3" = "--build" ]; then
+if [ $# -eq 2 ]; then
+	if [ "$2" = "--build" ]; then
 		# Build the docker container
 		docker build -t $TAG .
 	fi
 fi
 
 
+# docker run --gpus all -d -it -p $PORT:8888 -u $(id -u):$(id -g)
+docker run --gpus all -d -u $(id -u):$(id -g) -v $(pwd):/tf/main -p $PORT:8888 $TAG
 # Run the docker container. Add additional -v if
 # you need to mount more volumes into the container
 # Also, make sure to edit the ports to fix your needs.
-docker run --gpus all -d -it -p $PORT:8888 -v $(pwd):/home/jovyan/work \
-  -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes -e NB_UID="$(id -u)" \
-  -e NB_GID="$(id -g)" --user root --restart always --name $TAG \
-  semerulab/datascience:dev-cuda101
+# docker run --gpus all -d -it -p $PORT:8888 -v $(pwd):/home/jovyan/work \
+#  -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes -e NB_UID="$(id -u)" \
+#  -e NB_GID="$(id -g)" --user root --restart always --name $TAG \
+#  semerulab/datascience:dev-cuda101

c2037453710cbdbaad4e7baf39fbc574f1bdeec0
ncoop57 <nacooper01@email.wm.edu>
2021-02-12 18:42:14 +0000

    Update lib and docs

--- icodegen/_nbdev.py
+++ icodegen/_nbdev.py
@@ -2,7 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"remove_non_ascii": "00_data.core.ipynb",
+index = {"logger": "02_model.core.ipynb",
+         "URLs": "00_data.core.ipynb",
+         "remove_non_ascii": "00_data.core.ipynb",
          "beautify_code": "00_data.core.ipynb",
          "extra_tokens": "00_data.core.ipynb",
          "java_reserved_tokens": "00_data.core.ipynb",
@@ -13,6 +15,7 @@ index = {"remove_non_ascii": "00_data.core.ipynb",
          "replace_special_tokens": "00_data.core.ipynb",
          "train_tokenizer": "00_data.core.ipynb",
          "convert_df_to_tfds": "00_data.core.ipynb",
+         "process_data": "00_data.core.ipynb",
          "java_comment_remover": "01_data.transforms.ipynb",
          "line_randomizer": "01_data.transforms.ipynb",
          "code_token_randomizer": "01_data.transforms.ipynb",
@@ -20,10 +23,18 @@ index = {"remove_non_ascii": "00_data.core.ipynb",
          "Model": "02_model.core.ipynb",
          "TransformerModel": "02_model.core.ipynb",
          "RNNModel": "02_model.core.ipynb",
+         "VANILLA_CONFIG": "02_model.core.ipynb",
+         "GRU_CONFIG_1": "02_model.core.ipynb",
+         "GRU_CONFIG_2": "02_model.core.ipynb",
+         "GRU_CONFIG_3": "02_model.core.ipynb",
+         "GRU_CONFIG_4": "02_model.core.ipynb",
+         "GRU_CONFIG_5": "02_model.core.ipynb",
+         "train": "02_model.core.ipynb",
          "get_mean_probs": "04_evaluation.core.ipynb",
          "find_parens": "04_evaluation.core.ipynb",
          "mean_dist_probs": "04_evaluation.core.ipynb",
-         "get_mean_cross_entropy": "04_evaluation.core.ipynb"}
+         "get_mean_cross_entropy": "04_evaluation.core.ipynb",
+         "evaluate": "04_evaluation.core.ipynb"}
 
 modules = ["data/core.py",
            "data/transforms.py",
